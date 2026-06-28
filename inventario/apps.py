73e746dd245ac6e5b8dcc204f3a1972cc7c7d035
miguelez1997-jpg <miguelez1997@gmail.com>
2026-06-27 22:43:47 -0300

    Add files via upload

--- inventario/apps.py
+++ inventario/apps.py
@@ -1,5 +1,33 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def cargar_categorias_automaticas(sender, **kwargs):
+    
+    try:
+        from .models import Categoria
+        
+        lista_cat = [
+            "Tecnología y Electrónica", 
+            "Electrodomésticos", 
+            "Hogar y Muebles",
+            "Bazar y Cocina", 
+            "Herramientas y Ferretería", 
+            "Deportes y Fitness",
+            "Moda y Calzado", 
+            "Salud y Belleza", 
+            "Alimentos y Bebidas", 
+            "Librería y Oficina"
+        ]
+
+        for nombre in lista_cat:
+            Categoria.objects.get_or_create(nombre=nombre)
+            
+    except Exception:
+        pass
 
 class InventarioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventario'
+
+    def ready(self):
+        post_migrate.connect(cargar_categorias_automaticas, sender=self)
