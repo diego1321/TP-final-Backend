adc4092de6972c9cf44b93ff79c03f8eb8f5afe6
Juan Cruz Novoa <104076439+Jcnovoa1@users.noreply.github.com>
2026-06-29 11:08:19 -0300

    se crea el formulario ProductoForm con estilos de Bootstrap

--- inventario/forms.py
+++ inventario/forms.py
@@ -1,12 +1,22 @@
 from django import forms
-from .models import Producto
+from .models import Producto, Categoria
 
 class ProductoForm(forms.ModelForm):
+    cantidad_inicial = forms.IntegerField(
+        label="Stock Inicial", 
+        required=False,        
+        initial=0,             
+        min_value=0           
+    )
+
     class Meta:
         model = Producto
-        fields = ['nombre', 'precio']
+        fields = ['nombre', 'precio', 'categoria']
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
+        self.fields['categoria'].required = True
+        self.fields['categoria'].empty_label = "Seleccione una categoría"
+        
+        for field in self.fields.values():    
             field.widget.attrs['class'] = 'form-control'
